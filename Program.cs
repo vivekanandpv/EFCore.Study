112789f8f44373db5c03978578cf52ed8711f709
Vivekanand P V <vivekanandpv@outlook.com>
2023-02-03 21:51:36 +0530

    done

--- Program.cs
+++ Program.cs
@@ -5,9 +5,6 @@ namespace EFCore.Study;
 
 //  Instruction: Please populate the CustomerAddress table with some test data
 
-//  Step 1
-[Table("CustomerAddress")]
-[PrimaryKey("AddressId")]   //  since the primary key is non-conventional
 public class CustomerAddress
 {
     public int AddressId { get; set; }
@@ -17,28 +14,15 @@ public class CustomerAddress
     public string City { get; set; }
     public string StateCode { get; set; }
     public int Pin { get; set; }
-
-    //  conventional foreign key
     public int CustomerId { get; set; }
-
-    //  Navigation property (good to have)
-    //  One address belongs precisely to one customer
-    //  Eagerly loaded
     public Customer Customer { get; set; }
 }
 
-[Table("Customer")]
 public class Customer
 {
     public int CustomerId { get; set; }
     public string LegalName { get; set; }
     public string Gstin { get; set; }
-
-    //  Step 2
-    //  Navigation property
-    //  One customer has multiple addresses
-    //  You can also use ICollection<T>, HashSet<T>, List<T> here
-    //  Lazily loaded!
     public IList<CustomerAddress> Addresses { get; set; } = new List<CustomerAddress>();
 }
 
@@ -47,15 +31,25 @@ public class Customer
 public class SalesManagementContext : DbContext
 {
     public DbSet<Customer> Customers { get; set; }
-
-    //  Step 3
-    //  New DbSet for Addresses
     public DbSet<CustomerAddress> CustomerAddresses { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(
             @"Data Source=(localdb)\MSSQLLocalDb; Initial Catalog=SalesManagement;Integrated Security=true");
+    }
+
+    //  To customize the entity structure, we use OnModelCreating
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //base.OnModelCreating(modelBuilder); //  This is a no-op
+        modelBuilder.Entity<Customer>()
+            .ToTable("Customer");   //  This string value can be dynamically set,
+                                    //  which is not the case with attributes
+
+        modelBuilder.Entity<CustomerAddress>()
+            .ToTable("CustomerAddress")
+            .HasKey(ca => ca.AddressId);   //  Defining a non-conventional key.
     }
 }
 
@@ -78,9 +72,6 @@ internal class Program
 
         await context.Customers.ForEachAsync(c =>
         {
-            //  Step 4
-            //  The Addresses property of all customers
-            //  is null (if you did not initialize in Customer) or empty list
             Console.WriteLine($"{c.CustomerId} -> {c.LegalName}");
         });
     }
