0edf74685e5d3341fc2fe4498f55c15183ba1dba
Vivekanand P V <vivekanandpv@outlook.com>
2023-02-04 07:08:52 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EFCore.Study;
 
+//  Step 1
+//  Sometimes, you may wish to enable lazy-loading
+//  You want to load the related data, only on the first access
+//  To enable this, please install: Microsoft.EntityFrameworkCore.Proxies package
+//  and follow the below instructions
+
+//  This is not recommended for web applications
+
+//  Lazy loading can cause unneeded extra database roundtrips to occur
+//  (the so-called N+1 problem), and care should be taken to avoid this.
 public class CustomerAddress
 {
     public int AddressId { get; set; }
@@ -13,7 +24,10 @@ public class CustomerAddress
     public string StateCode { get; set; }
     public int Pin { get; set; }
     public int CustomerId { get; set; }
-    public Customer Customer { get; set; }
+
+    //  Step 2
+    //  related entities (one side or many side) should be marked as virtual
+    public virtual Customer Customer { get; set; }
 }
 
 public class Customer
@@ -21,7 +35,10 @@ public class Customer
     public int CustomerId { get; set; }
     public string LegalName { get; set; }
     public string Gstin { get; set; }
-    public IList<CustomerAddress> Addresses { get; set; } = new List<CustomerAddress>();
+
+    //  Step 3
+    //  related entities (one side or many side) should be marked as virtual
+    public virtual IList<CustomerAddress> Addresses { get; set; }
 }
 
 
@@ -33,7 +50,23 @@ public class SalesManagementContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(
+        optionsBuilder
+            //  Step 4
+            //  We enable lazy loading using this call
+            .UseLazyLoadingProxies()
+
+            //  Step 5
+            //  For logging the SQL, we use these methods
+            //  Logging the queries is only for development purposes
+            //  It's a severe vulnerability in production grade code
+            //  Use these methods with caution
+            .LogTo(Console.WriteLine, LogLevel.Information)
+
+            //  Step 6
+            //  Be extra careful using this
+            .EnableSensitiveDataLogging()
+
+            .UseSqlServer(
             @"Data Source=(localdb)\MSSQLLocalDb; Initial Catalog=SalesManagement;Integrated Security=true");
     }
 
@@ -63,13 +96,30 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        var context = new SalesManagementContext();
+        Console.WriteLine("--------------------Customer First-----------------------------");
 
-        //  Many side is not loaded into one side.
-        //  Also possible: Include("Addresses"), but not recommended
-        await context.Customers.Include(c => c.Addresses).ForEachAsync(c =>
+        using (var context = new SalesManagementContext())
         {
-            Console.WriteLine($"{c.CustomerId} -> {c.LegalName}");
-        });
+            //  Step 7
+            //  Only the customer is queried
+            var customer = await context.Customers.FirstAsync(c => c.CustomerId == 1);
+        
+            //  Step 8
+            //  Now the addresses are queried
+            var addresses = customer.Addresses;
+        }
+
+        Console.WriteLine("--------------------Address First-----------------------------");
+
+        using (var context = new SalesManagementContext())
+        {
+            //  Step 9
+            //  Only the address is queried
+            var address = await context.CustomerAddresses.FirstAsync(a => a.AddressId == 1);
+
+            //  Step 10
+            //  Now the customer is queried
+            var customer = address.Customer;
+        }
     }
 }
