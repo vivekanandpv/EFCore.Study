8244bf05610b1cd700af6b570945952f15f6949b
Vivekanand P V <vivekanandpv@outlook.com>
2023-02-04 12:13:59 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EFCore.Study;
 
@@ -35,7 +36,10 @@ public class SalesManagementContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(
+        optionsBuilder
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .UseSqlServer(
             @"Data Source=(localdb)\MSSQLLocalDb; Initial Catalog=SalesManagement;Integrated Security=true");
     }
 
@@ -70,9 +74,48 @@ internal class Program
     {
         using var context = new SalesManagementContext();
 
-        await context.Customers.ForEachAsync(c =>
+        //  The change tracker in the data context keeps track of all the property changes we make
+        //  To facilitate this, by default, all the entities are tracked
+        //  This obviously has a small overhead. If you think there is no need to track the entities
+        //  you can disable tracking by attaching AsNoTracking()
+        var customer = await context.Customers.FirstAsync(c => c.CustomerId == 1);
+
+        //  Because this entity is tracked, any change we make, is tracked
+        customer.Gstin = "HELLO1234";
+
+        //  getting all the changes the context has detected
+        var changes = context.ChangeTracker.Entries();
+
+
+        //  enumerating the changes
+        foreach (var entityEntry in changes)
         {
-            Console.WriteLine($"{c.CustomerId} -> {c.LegalName}");
-        });
+            Console.WriteLine($"Original Value: {entityEntry.OriginalValues[nameof(Customer.Gstin)]}");
+            Console.WriteLine($"Current Value: {entityEntry.CurrentValues[nameof(Customer.Gstin)]}");
+        }
+
+        //  Now that this change is tracked, we can save the changes if we wish
+        //  this will emit UPDATE statements on the database
+        int result = await context.SaveChangesAsync();  //  returns the number of records updated
+
+        Console.WriteLine($"We have updated {result} records");
+
+        //  To delete a record, first you need to fetch it
+        var address = await context.CustomerAddresses.FirstAsync();
+
+        //  Then, it has to be removed from the DbSet
+        context.CustomerAddresses.Remove(address);
+
+        //  enumerating the changes
+        var newChanges = context.ChangeTracker.Entries();
+
+        //  Ensuring the Customer is unchanged and address is deleted
+        foreach (var entityEntry in newChanges)
+        {
+            Console.WriteLine($"The entity {entityEntry.Entity} is: {entityEntry.State}");
+        }
+
+        //  If we save, it emits DELETE on the database
+        await context.SaveChangesAsync();
     }
 }
